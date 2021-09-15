7edcbf2af60f360e85d8cec29b9329595b265814
hiago-ramalho <hiagosantosramalho@gmail.com>
2021-09-15 08:20:39 -0300

    Adicionando Autenticação
    Adicionando Autenticação com :
    - next-auth
    - github (Oauth)

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
+import { signIn, useSession, signOut } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Hiago Ramalho
+      {session.user.name}
       <FiX color="#727280" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,18 +6,18 @@
 //toda vez q o usuário troca de tela, o _app.js é recarregado
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import '../styles/global.scss'
-{
-  /*todo estilo global deve estar dentro do _app.js */
-}
+/* todo estilo global deve estar dentro do _app.js */
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
